ba285e0f0dde15f0d03e7e513a5974d343126ab4
sonasiapps <sonasiapps@gmail.com>
2022-02-27 22:21:50 +0700

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ class HelloWorld
 	public static void main(String args[]){
 		System.out.println("Hello World!");
         System.out.println("Test Webhook!");
-        abc
+        System.out.println("Test Email Passed!");
 	}
 }
